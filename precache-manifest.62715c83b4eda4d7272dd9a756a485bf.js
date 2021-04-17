c828cc4a7395b9966a250aac95d4bfec7062d58b
Farshad Fahimi <farshadfahimi1371@gmail.com>
2021-04-17 23:56:15 +0430

    Updates

---
+++ precache-manifest.62715c83b4eda4d7272dd9a756a485bf.js
@@ -0,0 +1,86 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8a7aaad7148bb22d407f2477d698e78f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6d1748edf4e3604b6ed9",
+    "url": "/static/css/3.00373544.chunk.css"
+  },
+  {
+    "revision": "4f0ab31d0326087ef862",
+    "url": "/static/css/4.3e7d09e6.chunk.css"
+  },
+  {
+    "revision": "2924ac0e8bfac77b8448",
+    "url": "/static/css/main.207db4cf.chunk.css"
+  },
+  {
+    "revision": "01a024927071aff46e0a",
+    "url": "/static/js/2.071f533f.chunk.js"
+  },
+  {
+    "revision": "6e1d2efc12a1ac972ff1f942df7eb0bd",
+    "url": "/static/js/2.071f533f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6d1748edf4e3604b6ed9",
+    "url": "/static/js/3.85e74bc8.chunk.js"
+  },
+  {
+    "revision": "4f0ab31d0326087ef862",
+    "url": "/static/js/4.82ae7f59.chunk.js"
+  },
+  {
+    "revision": "2924ac0e8bfac77b8448",
+    "url": "/static/js/main.1e27a778.chunk.js"
+  },
+  {
+    "revision": "04a4050be8c489f59a7f",
+    "url": "/static/js/runtime-main.0a2456c5.js"
+  },
+  {
+    "revision": "21f233e19402cc4a66866a7f31191f0d",
+    "url": "/static/media/Agustina.21f233e1.woff"
+  },
+  {
+    "revision": "ee6539921d713482b8ccd4d0d23961bb",
+    "url": "/static/media/Montserrat-Regular.ee653992.ttf"
+  },
+  {
+    "revision": "47a7a717679b640ceb1b05d38c480a61",
+    "url": "/static/media/codeInLogo.47a7a717.webp"
+  },
+  {
+    "revision": "51cf7192327488da3d2a171280680c16",
+    "url": "/static/media/contactMailDark.51cf7192.svg"
+  },
+  {
+    "revision": "59389695208d7454c6607bed51ed4316",
+    "url": "/static/media/developerActivity.59389695.svg"
+  },
+  {
+    "revision": "e79c639294c805688be731921368c8f8",
+    "url": "/static/media/googleAssistantLogo.e79c6392.webp"
+  },
+  {
+    "revision": "c1a95f7335cbfb730b937e19e8830a53",
+    "url": "/static/media/programmer.c1a95f73.svg"
+  },
+  {
+    "revision": "ee62cb58630f29a6e6201b49d4c03728",
+    "url": "/static/media/pwaLogo.ee62cb58.webp"
+  },
+  {
+    "revision": "3440939881da8339d1597e8d77cdf850",
+    "url": "/static/media/skill.34409398.svg"
+  },
+  {
+    "revision": "13780cdd0144cacdef25486e2c2503f8",
+    "url": "/static/media/talksCardBack.13780cdd.svg"
+  },
+  {
+    "revision": "1fb7e17f3627608065b1b1921f5a7a47",
+    "url": "/static/media/unimates.1fb7e17f.png"
+  }
+]);
